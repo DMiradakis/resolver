d8d8db717acef8538b0896f8566b3b1d56691762
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-01-07 18:18:59 -0600

    Added version command to cli

---
+++ src/Resolver/Versioning/VersionService.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Resolver.Versioning
+{
+    public class VersionService
+    {
+        public static string GetVersion()
+        {
+            return Assembly
+                .GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion ?? "Unknown Version";
+        }
+    }
+}
