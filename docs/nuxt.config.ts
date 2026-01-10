44787911bdc6f72cb12c9d7d0ec8dc74bf221e01
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-01-10 15:46:36 -0600

    Added nuxt gtag

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
-    modules: ['@nuxt/ui'],
+    modules: ['@nuxt/ui', 'nuxt-gtag'],
     nitro: {
         preset: 'static'
     },
     site: {
         name: 'Resolver',
+    },
+    gtag: {
+        id: 'G-QLFG8NVHYX'
     }
 })
